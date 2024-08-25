65b5f321c258883cb78dd9c9d41c39f5c916e37b
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-08-25 14:34:10 +0200

    Update auf 1.1
    Wagen umgebaut auf Basis Mod
    - Sound
    - Zugschluss Tafeln angepasst
    - Fake hinzugefügt

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 ﻿function data()
 return {
 	info = {
-		minorVersion = 0,
+		minorVersion = 1,
 		severityAdd = "NONE",
 		severityRemove = "WARNING",
 		name = _("mod_name"),
@@ -17,12 +17,79 @@ return {
 		minGameVersion = 0,
 		dependencies = { },
 		url = { "" },
+	  
+		params = {
+			{
+				key = "fcs090fake",
+				name = _("Fake_fcs090_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_fcs090_wagen_desc"),
+				defaultIndex = 0,
+			},			
+			{
+				key = "soundCheck",
+				name = _("sound_check"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },	
+				tooltip = _("option_sound_check"),			
+				defaultIndex = 1,	
+			},
+        },
 	},
 	options = {
 	},
 	
-	runFn = function (settings)
-	
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
+
+        local hidden = {
+			["090_basf_fake.mdl"] = true,
+			["090_db_fake.mdl"] = true,
+			["090_quarzwerke_fake.mdl"] = true,
+			["090_rag_fake.mdl"] = true,
+			["090_basf_fake.mdl"] = true,
+			["090_db_fake.mdl"] = true,
+			["090_dbag_fake.mdl"] = true,
+			["090_quarzwerke_fake.mdl"] = true,
+			["090_rag_fake.mdl"] = true,
+			["092_db_fake.mdl"] = true,
+			["092_dbag_fake.mdl"] = true,
+			["092_dbcargo_fake.mdl"] = true,
+			["70_db_fake.mdl"] = true,
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/Eds([^/]*.mdl)')
+							or fileName:match('/Fcs([^/]*.mdl)')
+							or fileName:match('/Otmm([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["fcs090fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+		
+		local function metadataHandler(fileName, data)
+			if params.soundCheck == 0 then
+				if fileName:match('/vehicle/waggon/Fcs090/Eds([^/]*.mdl)') 
+				or fileName:match('/vehicle/waggon/Fcs090/Fcs([^/]*.mdl)')  
+				or fileName:match('/vehicle/waggon/Fcs090/Otmm([^/]*.mdl)')    
+				or fileName:match('/vehicle/waggon/Fcs090/Fake/Eds([^/]*.mdl)') 
+				or fileName:match('/vehicle/waggon/Fcs090/Fake/Fcs([^/]*.mdl)')  
+				or fileName:match('/vehicle/waggon/Fcs090/Fake/Otmm([^/]*.mdl)') 
+				then
+					data.metadata.railVehicle.soundSet.name = "waggon_freight_old"
+				end
+			end
+			return data
+		end
+
+		addModifier( "loadModel", metadataHandler )
 	end
 	}
 end
--- strings.lua
+++ strings.lua
@@ -4,6 +4,12 @@ return {
 		["mod_name"] = "Otmm 70 | Eds 090 | Fcs 090 | Fcs 092",
 		["mod_desc"] = "The Otmm 70 (from 1964: Eds 090, from 1980: Fcs 090) is a two-axle self-unloading wagon. This two-axle open freight wagon is used for the transport of bulk goods that are not sensitive to moisture and is the most frequently built self-unloading wagon of the DB with high, meterable and gravity discharge on both sides. These freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
 
+        ["Fake_fcs090_wagen"] = "Enable Fcs 090 Fake Versions?",
+        ["option_fake_fcs090_wagen_desc"] = "Should the fake versions of the Fcs 090 be available in the game?",
+		
+		["sound_check"] = "Activate custom sound?",
+		["option_sound_check"] = "Should the extra sound be activated?",
+				
         ["Fcs090_menu"] = "Otmm 70 | Eds 090 | Fcs 090 | Fcs 092",
 		["Otmm70_db_name"] = "Otmm 70 DB (1961)",
 		["Otmm70_db_desc"] = "The first wagons were built from 1961 onwards, with series production starting in 1962. The freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
@@ -15,8 +21,6 @@ return {
 		["Fcs090_db_name"] = "Fcs 090 DB (1980)",
         ["Fcs090_basf_name"] = "Fcs 090 BASF (1980)",
         ["Fcs090_quarzwerke_name"] = "Fcs 090 Quarzwerke (1980)",
-        ["Fcs090_gleisbau_name"] = "Fcs 090 DB Gleisbau (1980)",
-        ["Fcs090_wiebe_name"] = "Fcs 090 H.F. Wiebe (1980)",
         ["Fcs090_rag_name"] = "Fcs 090 Ruhrkohle AG (1980)",
 		["Fcs090_db_desc"] = "After the brakes were rebuilt, the wagons were listed as Fcs 090. The freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
 		["Fcs090_dbag_name"] = "Fcs 090 DB AG (1994)",
@@ -27,11 +31,35 @@ return {
 		["Fcs092_dbag_desc"] = "The refurbished Fcs 092 wagons were painted in DB AG's traffic red, the valid colour scheme since 1996.",
         ["Fcs092_dbcargo_name"] = "Fcs 092 DB Cargo (1999)",
 		["Fcs092_dbcargo_desc"] = "An Fcs 092 in traffic red with DB Cargo lettering.",
+		
+		--- Fake ---
+		
+		["Fcs090_menu_fake"] = "Otmm 70 | Eds 090 | Fcs 090 | Fcs 092 Fake",
+		
+		["Otmm70_db_name_fake"] = "Otmm 70 DB (1961) Fake",
+		["Eds090_db_name_fake"] = "Eds 090 DB (1964) Fake",
+		["Eds090_basf_name_fake"] = "Eds 090 BASF (1964) Fake",
+		["Eds090_quarzwerke_name_fake"] = "Eds 090 Quarzwerke (1964) Fake",
+		["Eds090_rag_name_fake"] = "Eds 090 Ruhrkohle AG (1968) Fake",
+		["Fcs090_db_name_fake"] = "Fcs 090 DB (1980) Fake",
+        ["Fcs090_basf_name_fake"] = "Fcs 090 BASF (1980) Fake",
+        ["Fcs090_quarzwerke_name_fake"] = "Fcs 090 Quarzwerke (1980) Fake",
+        ["Fcs090_rag_name_fake"] = "Fcs 090 Ruhrkohle AG (1980) Fake",
+		["Fcs090_dbag_name_fake"] = "Fcs 090 DB AG (1994) Fake",
+		["Fcs092_db_name_fake"] = "Fcs 092 DB (1991) Fake",
+		["Fcs092_dbag_name_fake"] = "Fcs 092 DB AG (1996) Fake",
+		["Fcs092_dbcargo_name_fake"] = "Fcs 092 DB Cargo (1999) Fake",	
 	},
 	de = {
 		["mod_name"] = "Otmm 70 | Eds 090 | Fcs 090 | Fcs 092",
 		["mod_desc"] = "Der Otmm 70 (ab 1964: Eds 090, ab 1980: Fcs 090) ist ein zweiachsiger Selbstentladewagen. Dieser zweiachsige offene Güterwagen wird zum Transport von nässeunempfindlichen Schüttgütern eingesetzt und ist mit hochliegender, dosierbarer und beidseitiger Schwerkraftentleerung der meistgebaute Selbstentladewagen der DB. Diese Güterwagen sind geeignet zur Beförderung vom Ladegut wie Sand, Kies, Erz oder Kohle.",
 
+        ["Fake_fcs090_wagen"] = "Fcs 090 Fake-Versionen aktivieren?",
+        ["option_fake_fcs090_wagen_desc"] = "Sollen die Fake-Versionen der Fcs 090 verfügbar sein im Spiel?",
+
+		["sound_check"] = "Eigenes Soundset aktivieren?",
+		["option_sound_check"] = "Soll der extra Sound aktiviert sein?",
+		
         ["Fcs090_menu"] = "Otmm 70 | Eds 090 | Fcs 090 | Fcs 092",
 		["Otmm70_db_name"] = "Otmm 70 DB (1961)",
 		["Otmm70_db_desc"] = "Die ersten Wagen wurden ab 1961 gebaut, die Serienfertigung erfolgte ab 1962. Die Güterwagen sind geeignet zur Beförderung vom Ladegut wie Sand, Kies, Erz oder Kohle.",
@@ -43,8 +71,6 @@ return {
 		["Fcs090_db_name"] = "Fcs 090 DB (1980)",
         ["Fcs090_basf_name"] = "Fcs 090 BASF (1980)",
         ["Fcs090_quarzwerke_name"] = "Fcs 090 Quarzwerke (1980)",
-        ["Fcs090_gleisbau_name"] = "Fcs 090 DB Gleisbau (1980)",
-        ["Fcs090_wiebe_name"] = "Fcs 090 H.F. Wiebe (1980)",
         ["Fcs090_rag_name"] = "Fcs 090 Ruhrkohle AG (1980)",
 		["Fcs090_db_desc"] = "Nach Umbau der Bremsen wurden die Wagen als Fcs 090 geführt. Die Güterwagen sind geeignet zur Beförderung vom Ladegut wie Sand, Kies, Erz oder Kohle.",
 		["Fcs090_dbag_name"] = "Fcs 090 DB AG (1994)",
@@ -55,8 +81,24 @@ return {
 		["Fcs092_dbag_desc"] = "Die hergerichteten Wagen der Gattung Fcs 092 wurde im verkehrsrot der DB AG lackiert, dem gültigen Farbschema seit 1996.",
         ["Fcs092_dbcargo_name"] = "Fcs 092 DB Cargo (1999)",
 		["Fcs092_dbcargo_desc"] = "Ein Fcs 092 in verkehrsrot mit Beschriftung von DB Cargo.",
-        ["Schotterwagen_oebb_name"] = "Fcs 092 ÖBB Infrastruktur (2007)",
-        ["Schotterwagen_oebb_desc"] = "Schotterwagen der ÖBB Infrastruktur.",        
+
+		---	Fake ---
+		
+		["Fcs090_menu_fake"] = "Otmm 70 | Eds 090 | Fcs 090 | Fcs 092 Fake",
+		
+		["Otmm70_db_name_fake"] = "Otmm 70 DB (1961) Fake",
+		["Eds090_db_name_fake"] = "Eds 090 DB (1964) Fake",
+		["Eds090_basf_name_fake"] = "Eds 090 BASF (1964) Fake",
+		["Eds090_quarzwerke_name_fake"] = "Eds 090 Quarzwerke (1964) Fake",
+		["Eds090_rag_name_fake"] = "Eds 090 Ruhrkohle AG (1968) Fake",
+		["Fcs090_db_name_fake"] = "Fcs 090 DB (1980) Fake",
+        ["Fcs090_basf_name_fake"] = "Fcs 090 BASF (1980) Fake",
+        ["Fcs090_quarzwerke_name_fake"] = "Fcs 090 Quarzwerke (1980) Fake",
+        ["Fcs090_rag_name_fake"] = "Fcs 090 Ruhrkohle AG (1980) Fake",
+		["Fcs090_dbag_name_fake"] = "Fcs 090 DB AG (1994) Fake",
+		["Fcs092_db_name_fake"] = "Fcs 092 DB (1991) Fake",
+		["Fcs092_dbag_name_fake"] = "Fcs 092 DB AG (1996) Fake",
+		["Fcs092_dbcargo_name_fake"] = "Fcs 092 DB Cargo (1999) Fake",	
 	},
 }
 end
